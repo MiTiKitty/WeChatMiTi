3dddabc298daed622d830afc8c20957fe6d79789
CatKitty <3364187323@qq.com>
2023-02-27 09:11:17 +0800

    完成time07的作业

---
+++ pages/time07/time07.js
@@ -0,0 +1,109 @@
+// pages/time07/time07.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgs: [{
+        src: "https://img1.baidu.com/it/u=1605341541,1182642759&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=750"
+      },
+      {
+        src: "https://img2.baidu.com/it/u=3202947311,1179654885&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500"
+      },
+      {
+        src: "https://img1.baidu.com/it/u=1308526554,393095433&fm=253&fmt=auto&app=120&f=JPEG?w=1280&h=800"
+      },
+      {
+        src: "https://img2.baidu.com/it/u=1003272215,1878948666&fm=253&fmt=auto&app=120&f=JPEG?w=1280&h=800"
+      }
+    ],
+    users: [{
+        avatar: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F1009f926-f515-4b2a-8489-a2e9a5a1fc1d%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1680051591&t=4c1189e96a4082d8da348b69ab4ee6fe",
+        name: '蔡徐坤',
+        age: 24,
+        description: '1998年8月2日出生于浙江省温州市，户籍湖南吉首，中国内地男歌手、演员、原创音乐制作人、MV导演。'
+      },
+      {
+        avatar: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2Fe50ac1b6-3d7a-4943-8141-1dff3f16c045%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1680052118&t=a2b363d257870d2462f17886b85d8fea",
+        name: '蔡徐坤',
+        age: 24,
+        description: '1998年8月2日出生于浙江省温州市，户籍湖南吉首，中国内地男歌手、演员、原创音乐制作人、MV导演。'
+      },
+      {
+        avatar: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F9d9eb56a-a6ff-47b9-9ea2-08a7a6ac5a02%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1680052118&t=a6279a8f14bfd21a0e3340c103ac3360",
+        name: '蔡徐坤',
+        age: 24,
+        description: '1998年8月2日出生于浙江省温州市，户籍湖南吉首，中国内地男歌手、演员、原创音乐制作人、MV导演。'
+      },
+      {
+        avatar: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2Fcabbd418-26bc-4354-93ac-3c87ea425b20%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1680052118&t=2a6fbbcab6f2a3d44480e661859c6dc6",
+        name: '蔡徐坤',
+        age: 24,
+        description: '1998年8月2日出生于浙江省温州市，户籍湖南吉首，中国内地男歌手、演员、原创音乐制作人、MV导演。'
+      },
+      {
+        avatar: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F4c071ebe-062e-4ba1-862a-3c9f28a56fdb%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1680052118&t=0209e45fcbdca40cd28fca148c1d5383",
+        name: '蔡徐坤',
+        age: 24,
+        description: '1998年8月2日出生于浙江省温州市，户籍湖南吉首，中国内地男歌手、演员、原创音乐制作人、MV导演。'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
